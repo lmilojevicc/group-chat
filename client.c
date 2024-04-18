d7cc611bdb3fe0e8388032cac23fe6a561b9d55b
Luka <milojevic.luka2002@gmail.com>
2024-04-19 00:55:28 +0200

    Created simple client that sends message to the server

--- client.c
+++ client.c
@@ -1,4 +1,21 @@
+#include "utils/socket.h"
+#define PORT 9999
+#define IP ""
+
 int main(int argc, char const* argv[]) {
+	int clientSocketFD = createSocket();
+	struct sockaddr_in* serverAddress = createAddress(IP, PORT);
+
+	int connectResult = connect(clientSocketFD, (struct sockaddr*)serverAddress, sizeof(*serverAddress));
+	if (connectResult == 0) {
+		printf("Successfully connected to the server\n");
+	}
+
+	char* message;
+
+	message = "Hello server\n";
+	send(clientSocketFD, message, strlen(message), 0);
+	printf("Message sent to server\n");
 
 	return 0;
 }
