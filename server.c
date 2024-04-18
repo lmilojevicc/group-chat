ae18d8c66d1a09bbeb12ca6fea115091a824b4a6
Luka <milojevic.luka2002@gmail.com>
2024-04-19 00:55:49 +0200

    Created a server that receives message from client

--- server.c
+++ server.c
@@ -1,27 +1,32 @@
 #include "utils/socket.h"
+#include <stdbool.h>
+#define PORT 9999
+#define IP ""
 
 int main(int argc, char const* argv[]) {
 	int serverSocketFD = createSocket();
-	struct sockaddr_in* serverAddress = createAddress("", 9999);
+	const struct sockaddr_in* serverAddress = createAddress(IP, PORT);
 
-	int bindResult = bind(serverSocketFD, serverAddress, sizeof(*serverAddress));
+	int bindResult = bind(serverSocketFD, (struct sockaddr*)serverAddress, sizeof(*serverAddress));
 	if (bindResult == 0) {
-		printf("Socket bound successfully");
+		printf("Socket bound successfully\n");
 	}
 
 	int listenResult = listen(serverSocketFD, 10);
 	if (listenResult == 0) {
-		printf("Accepted a client");
+		printf("Server is listening on %d\n", PORT);
 	}
 
-	struct sockaddr_in* clientAddress;
-	int clientAddressSize = sizeof(clientAddress);
+	struct sockaddr_in clientAddress;
+	socklen_t clientAddressSize = (socklen_t)sizeof(clientAddress);
+	int clientFD = accept(serverSocketFD, (struct sockaddr*)&clientAddress, &clientAddressSize);
 
-	int clientFD = accept(serverSocketFD, clientAddress, &clientAddressSize);
+	printf("Client connected successfully\n");
 
-	char* message;
-	message = "Server message";
-	send(clientFD, message, sizeof(message), 0);
+	char buffer[1024];
+	memset(buffer, 0, sizeof(buffer));
+	recv(clientFD, buffer, sizeof(buffer), 0);
+	printf("Message from the client: %s\n", buffer);
 
 	return 0;
 }
