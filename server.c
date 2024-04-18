bd6b67b3100bb4b014c35234735c46063eeb0204
Luka <milojevic.luka2002@gmail.com>
2024-04-18 23:04:20 +0200

    Created a basic server that sends a message to a client

--- server.c
+++ server.c
@@ -1,4 +1,27 @@
+#include "utils/socket.h"
+
 int main(int argc, char const* argv[]) {
+	int serverSocketFD = createSocket();
+	struct sockaddr_in* serverAddress = createAddress("", 9999);
+
+	int bindResult = bind(serverSocketFD, serverAddress, sizeof(*serverAddress));
+	if (bindResult == 0) {
+		printf("Socket bound successfully");
+	}
+
+	int listenResult = listen(serverSocketFD, 10);
+	if (listenResult == 0) {
+		printf("Accepted a client");
+	}
+
+	struct sockaddr_in* clientAddress;
+	int clientAddressSize = sizeof(clientAddress);
+
+	int clientFD = accept(serverSocketFD, clientAddress, &clientAddressSize);
+
+	char* message;
+	message = "Server message";
+	send(clientFD, message, sizeof(message), 0);
 
 	return 0;
 }
