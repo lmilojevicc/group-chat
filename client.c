52b8a78f1113658170040c48d7b1f48c4214dda9
Luka <milojevic.luka2002@gmail.com>
2024-04-20 01:52:16 +0200

    Enabled client to accept messages from server

--- client.c
+++ client.c
@@ -1,4 +1,5 @@
 #include "utils/socket.h"
+#include <pthread.h>
 #include <stdbool.h>
 #include <time.h>
 #include <unistd.h>
@@ -6,13 +7,47 @@
 #define PORT 9999
 #define IP ""
 
+struct ClientData {
+	int clientSocketFD;
+	char name[100];
+};
+
+void* messageReceiver(void* arg) {
+	struct ClientData* client = (struct ClientData*)arg;
+	int clientSocketFD = client->clientSocketFD;
+	char* name = client->name;
+	char broadcastBuffer[1024];
+
+	while (true) {
+		ssize_t charReceived = recv(clientSocketFD, broadcastBuffer, sizeof(broadcastBuffer), 0);
+		if (charReceived > 0) {
+			broadcastBuffer[charReceived] = 0;
+			printf("\r%s\n%s:", broadcastBuffer, name);
+			fflush(stdout);
+		} else if (charReceived == 0) {
+			break;
+		}
+	}
+
+	close(clientSocketFD);
+	free(client);
+
+	return NULL;
+}
+
+void messageReceiverThread(struct ClientData* clientData) {
+	pthread_t id;
+	pthread_create(&id, NULL, messageReceiver, (void*)clientData);
+}
+
 int main(int argc, char const* argv[]) {
 	int clientSocketFD = createSocket();
 	struct sockaddr_in* serverAddress = createAddress(IP, PORT);
 
 	int connectResult = connect(clientSocketFD, (struct sockaddr*)serverAddress, sizeof(*serverAddress));
-	if (connectResult == 0) {
-		printf("Successfully connected to the server\n");
+	if (connectResult != 0) {
+		printf("Failed to connect to the server\n");
+		exit(1);
 	}
 
 	char name[100];
@@ -30,9 +65,15 @@ int main(int argc, char const* argv[]) {
 
 	send(clientSocketFD, name, strlen(name), 0);
 
+	struct ClientData* clientData = malloc(sizeof(struct ClientData));
+	clientData->clientSocketFD = clientSocketFD;
+	strcpy(clientData->name, name);
+
 	char* line = NULL;
 	size_t lineSize = 0;
 	printf("Hi %s, you can now send messages to the group chat.\n\nType EXIT to leave the chat\n", name);
+
+	messageReceiverThread(clientData);
 
 	while (true) {
 		printf("%s:", name);
